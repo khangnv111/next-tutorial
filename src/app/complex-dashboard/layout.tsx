b40fbeb2e6bac14f00ab16776e7637bf0a2a0554
Dominic Nguyenx <khangnv111@gmail.com>
2025-11-03 22:40:35 +0700

    32 - Conditional Routes

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -11,7 +11,7 @@ export default function ComplexDashboardLayout({
     notifications: React.ReactNode;
     login: React.ReactNode;
 }) {
-    const isLoggedIn = true;
+    const isLoggedIn = false;
     return isLoggedIn ? (
         <div>
             <div>{children}</div>
