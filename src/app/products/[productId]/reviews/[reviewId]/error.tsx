c7c0facb4921ed2686e87f8c410fa2b66321cd1a
Dominic Nguyenx <khangnv111@gmail.com>
2025-11-03 21:01:55 +0700

    26 - Recovering from Errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,10 +1,26 @@
 "use client";
 
-export  default  function ErrorBoundary({ error }: { error: Error }) {
+import {useRouter} from "next/navigation";
+import {startTransition} from "react";
+
+export  default  function ErrorBoundary({ error, reset }:
+{
+    error: Error,
+    reset: () => void
+}) {
+    const router = useRouter();
+    const reload = () => {
+        startTransition(() => {
+            router.refresh();
+            reset();
+        })
+    }
+
     return (
         <>
             <h2>Something went wrong while loading the review.</h2>
             <p>{error.message}</p>
+            <button onClick={() => reload()}> Try again </button>
         </>
     );
 }
